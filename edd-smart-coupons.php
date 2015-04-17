ecd3c6afcccd5f0966afbb553fa511508fdd6cf2
Andreea Bardasu <andreea.bardasu@gmail.com>
2015-04-17 14:19:09 +0300

    Updated hook priority to supersede that of EDD (#177)

--- edd-smart-coupons.php
+++ edd-smart-coupons.php
@@ -13,17 +13,15 @@ define( 'EDD_SMART_COUPONS_ARGUMENT', 'show_coupon' );
 define( 'EDD_SMART_COUPONS_COOKIE', 'edd_smart_coupons_enabled' );
 define( 'EDD_SMART_COUPONS_EXPIRE', time() + ( 60 * 60 * 24 * 365 ) ); // now + 1 year
 
-function edd_smart_coupons_request( $request ) {
-	if ( ! empty( $_REQUEST[ EDD_SMART_COUPONS_ARGUMENT ] ) ) {
+function edd_smart_coupons_request() {
+	if ( ! empty( $_REQUEST[ EDD_SMART_COUPONS_ARGUMENT ] ) && empty( $_COOKIE[ EDD_SMART_COUPONS_COOKIE ] ) ) {
 		setcookie( EDD_SMART_COUPONS_COOKIE, true, EDD_SMART_COUPONS_EXPIRE );
 		wp_redirect( remove_query_arg( EDD_SMART_COUPONS_ARGUMENT ) );
 		exit;
 	}
-
-	return $request;
 }
 
-add_action( 'request', 'edd_smart_coupons_request' );
+add_action( 'init', 'edd_smart_coupons_request', -1 );
 
 function edd_smart_coupons_styles() {
 	if ( empty( $_COOKIE[ EDD_SMART_COUPONS_COOKIE ] ) ) {
