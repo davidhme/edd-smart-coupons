36e454018a1bc641e230a32a257489e49a921b42
Andreea Bardasu <andreea.bardasu@gmail.com>
2015-04-23 17:19:18 +0300

    Moved cookie code on client (#177)

--- edd-smart-coupons.php
+++ edd-smart-coupons.php
@@ -13,16 +13,7 @@ define( 'EDD_SMART_COUPONS_ARGUMENT', 'discount' );
 define( 'EDD_SMART_COUPONS_COOKIE', 'edd_smart_coupons_enabled' );
 define( 'EDD_SMART_COUPONS_EXPIRE', time() + ( 60 * 60 * 24 * 365 ) ); // now + 1 year
 
-function edd_smart_coupons_request() {
-	if ( ! empty( $_REQUEST[ EDD_SMART_COUPONS_ARGUMENT ] ) && empty( $_COOKIE[ EDD_SMART_COUPONS_COOKIE ] ) ) {
-		$_COOKIE[ EDD_SMART_COUPONS_COOKIE ] = true;
-		setcookie( EDD_SMART_COUPONS_COOKIE, true, EDD_SMART_COUPONS_EXPIRE );
-	}
-}
-
-add_action( 'init', 'edd_smart_coupons_request', -1 );
-
-function edd_smart_coupons_styles() {
+function edd_smart_coupons_setup() {
 	if ( empty( $_COOKIE[ EDD_SMART_COUPONS_COOKIE ] ) ) {
 		?>
 		<style>
@@ -30,8 +21,27 @@ function edd_smart_coupons_styles() {
 				display: none;
 			}
 		</style>
+		<script>
+			if ( window.location.href.match( <?php echo '/[?&]' . EDD_SMART_COUPONS_ARGUMENT . '=/' ?> ) ) {
+				document.cookie =
+					'<?php echo EDD_SMART_COUPONS_COOKIE ?>=1; ' +
+					'expires=' + new Date(<?php echo EDD_SMART_COUPONS_EXPIRE * 1000 ?>).toUTCString() + '; ' +
+					'path=/';
+
+				var params = {};
+
+				window.location.search.slice( 1 ).split( '&' ).forEach(function( part ) {
+					var param = part.split( '=' );
+					if ( param[0] != '<?php echo EDD_SMART_COUPONS_ARGUMENT ?>' ) {
+						params[ param[0] ] = param[1];
+					}
+				});
+
+				window.location.href = ( window.location.pathname + '?' + jQuery.param( params ) ).replace( /\?$/, '' );
+			}
+		</script>
 		<?php
 	}
 }
 
-add_action( 'wp_head', 'edd_smart_coupons_styles' );
+add_action( 'wp_head', 'edd_smart_coupons_setup' );
